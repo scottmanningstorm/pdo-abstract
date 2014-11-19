dab6825d3fe1bda3487d0ff3b8bd5c6feee70057
Scott Manning <scottm@stormcreative.co.uk>
2014-11-19 18:14:26 +0000

    Updated PDO

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 //   DEBUG - Error Reporting. //
 error_reporting(E_ALL);   	  //
 ini_set('display_errors', 1); //
+ini_set('allow_url_include', 1); 						  //
 ////////////////////////////////
 
 require_once('../penguin-framwork/loader/autoLoader.php'); 
 
 AutoLoader::findClass(); 
  
-$test = new Active('user');
+
+
+//$test = new Active('user');
 
 /////////////////
 ///TEST INDEX//// 
@@ -25,9 +28,10 @@ var_dump( $test->where('id', '=', 173)->get() );
 
 $route = new Router(); 
 
+$model = new Home(); 
 //$route->dumpURI(); 
 	 
-
+ 
 echo '<br /> <br /> <br /> <br /> ';
 
 
